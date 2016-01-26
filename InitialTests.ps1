6df3e8c16a9c6023bd1ff8b8e0ac3fb09f130e9d
unknown <adm-rcerda@robjump.corp.netflix.com>
2016-01-25 23:21:59 -0800

    Added auth token and invoke-restmethod

--- InitialTests.ps1
+++ InitialTests.ps1
@@ -1,1 +1,7 @@
-﻿
+﻿$PagerDutySubDomain = 'Netflix'
+$Token = Get-Content C:\Admin\Pagerduty\Token.txt
+$query = $null
+
+$result = Invoke-RestMethod -Method GET -Uri ('https://' + $PagerDutySubDomain + '.pagerduty.com/api/v1/users' + $query) -ContentType "application/json" -Headers @{"Authorization"=("Token token=" + $Token)}
+
+
